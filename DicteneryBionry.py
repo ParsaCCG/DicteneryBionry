683e2f5396ed124d38f928bdee14c92193fe25b9
ParsaCCG <jparsa1387@gmail.com>
2022-09-02 22:18:28 +0430

    Add Project

---
+++ DicteneryBionry.py
@@ -0,0 +1,85 @@
+print("Hello To App Text To Binery! ")
+asd = input("Please Enter Your Text: ")
+ad = {
+"a": "01100001",
+"b": "01100010",
+"c": "01100011",
+"d": "01100100",
+"e": "01100101",
+"f": "01100110",
+"g": "01100111",
+"h": "01101000",
+"i": "01101001",
+"j": "01101010",
+"k": "01101011",
+"l": "01101100",
+"m": "01101101",
+"n": "01101110",
+"o": "01101111",
+"p": "01110000",
+"q": "01110001",
+"r": "01110010",
+"s": "01110011",
+"t": "01110100",
+"u": "01110101",
+"v": "01110110",
+"w": "01110111",
+"x": "01111000",
+"y": "01111001",
+"z": "01111010"
+}
+
+if asd == "a":
+    print(ad.get("a"))
+elif asd == "b":
+    print(ad.get("b"))
+elif asd == "c":
+    print(ad.get("c"))
+elif asd == "c":
+    print(ad.get("c"))
+elif asd == "d":
+    print(ad.get("d"))
+elif asd == "e":
+    print(ad.get("e"))
+elif asd == "f":
+    print(ad.get("f"))
+elif asd == "g":
+    print(ad.get("g"))
+elif asd == "h":
+    print(ad.get("h"))
+elif asd == "i":
+    print(ad.get("i"))
+elif asd == "j":
+    print(ad.get("j"))
+elif asd == "k":
+    print(ad.get("k"))
+elif asd == "l":
+    print(ad.get("l"))
+elif asd == "m":
+    print(ad.get("m"))
+elif asd == "n":
+    print(ad.get("n"))
+elif asd == "o":
+    print(ad.get("o"))
+elif asd == "p":
+    print(ad.get("p"))
+elif asd == "q":
+    print(ad.get("q"))
+elif asd == "r":
+    print(ad.get("r"))
+elif asd == "s":
+    print(ad.get("s"))
+elif asd == "t":
+    print(ad.get("t"))
+elif asd == "u":
+    print(ad.get("u"))
+elif asd == "v":
+    print(ad.get("v"))
+elif asd == "w":
+    print(ad.get("w"))
+elif asd == "x":
+    print(ad.get("x"))
+elif asd == "y":
+    print(ad.get("y"))
+elif asd == "z":
+    print(ad.get("z"))
